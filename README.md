201d0295eafb2d2722e7f72142154cb5bb9d3ca0
greggameplayer <gregoire.hage@gmail.com>
2022-11-09 00:43:44 +0100

    first version

---
+++ README.md
@@ -0,0 +1,5 @@
+Unbound YT Ads Blocker
+----------------
+This project is made for unbound to be able to block ads on YouTube with use of Ansible.
+
+Credits : [mkaand](https://github.com/mkaand) for base inspiration with his [project](https://github.com/mkaand/youtube-ads-blocker-ddwrt)
---
+++ roles/common/files/generate-unbound-rules.py
@@ -0,0 +1,20 @@
+import requests
+
+url = 'https://raw.githubusercontent.com/anudeepND/youtubeadsblacklist/master/domainlist.txt'
+r = requests.get(url)
+r.raise_for_status()
+
+print('server:')
+
+for line in r.text.splitlines():
+    print('  local-zone: "{}" redirect'.format(line))
+    print('  local-data: "{} A 127.0.0.1"'.format(line))
+
+url = 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com'
+r = requests.get(url)
+r.raise_for_status()
+
+for line in r.text.splitlines():
+    print('  local-zone: "{}" redirect'.format(line))
+    print('  local-data: "{} A 127.0.0.1"'.format(line.split(',')[0]))
+
